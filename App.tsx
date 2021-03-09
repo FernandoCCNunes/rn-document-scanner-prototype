5f8438610c2bef94318d4344cfeb91db28cf4d6d
Fernando Nunes <fernandocruznunes@enta.pt>
2021-03-09 11:45:21 -0100

    Init Project

---
+++ App.tsx
@@ -0,0 +1,48 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { Component } from "react";
+import {
+    SafeAreaView,
+    StatusBar
+} from "react-native";
+
+interface DocumentScannerPrototypeProps {
+
+}
+
+interface DocumentScannerPrototypeState {
+
+}
+
+class DocumentScannerPrototype extends Component<DocumentScannerPrototypeProps, DocumentScannerPrototypeState> {
+
+
+    constructor(props: DocumentScannerPrototypeProps) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+
+                </SafeAreaView>
+            </>
+        )
+    }
+}
+
+export default DocumentScannerPrototype;
